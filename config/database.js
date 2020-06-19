249b7500358557e556da1b69a56ceaaeb182e70f
diana shelotkach <diana.shelotkach@mail.ru>
2020-06-19 04:11:16 +0300

    Initial commit

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 module.exports = {
     'connection': {
         'host': 'd9c88q3e09w6fdb2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-        'user': 'p0qrka4yi6779dt9',
-        'password': 'x16pi9379hfknlwl',
+        'user': 'qgq2xz22dtcxz3e9',
+        'password': 'lxr0l010d0tya7v5',
         'port': '3306'
     },
-    'database': 'df0r79uq0neadtvy',
+    'database': 'kw8z3e2bkedtzgjx',
     'users_table': 'users'
 };
