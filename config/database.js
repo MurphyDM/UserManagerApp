cdba608422005088a83418f4ebe48d470e4ee4d5
diana shelotkach <diana.shelotkach@mail.ru>
2020-06-20 22:59:55 +0300

    Updated database password

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ module.exports = {
     'connection': {
         'host': 'd9c88q3e09w6fdb2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
         'user': 'qgq2xz22dtcxz3e9',
-        'password': 'uwgu22wfcm8ll5if',
+        'password': 'y3yly2lblaoxcqoc',
         'port': '3306'
     },
     'database': 'kw8z3e2bkedtzgjx',
